bc7aec7eeb261d4875b39e36fbcd76916d009e9e
고종환 <jonghwankoh@ajou.ac.kr>
2023-11-29 10:09:27 +0900

    minor update

--- clipdropTest.py
+++ clipdropTest.py
@@ -13,7 +13,7 @@ image_file_object = buffer.tobytes()
 bg_removed_img = cv2.imread("mask.png", cv2.IMREAD_UNCHANGED)
 mask_img = np.where(bg_removed_img[..., 3] >= 63, 0, 255).astype(np.uint8)
 kernel = np.ones((10, 10), np.uint8)
-eroded_mask = cv2.erode(mask_img, kernel, iterations=1)
+eroded_mask = cv2.erode(mask_img, kernel, iterations=2)
 inverted_mask = cv2.bitwise_not(eroded_mask)
 cv2.imwrite("result.png", inverted_mask)
 
